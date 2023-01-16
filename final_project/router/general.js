7e98c71d3b862f4710f2f938fa0d83712ecdc639
Xoror <c.inderbitzi@outlook.com>
2023-01-16 07:56:00 -0500

    final changes

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,33 +12,59 @@ public_users.get('/',function (req, res) {
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
     const isbn = req.params.isbn;
-    res.send(books[isbn])
+    if (!books[isbn]) {
+        return res.status(404).json({message: "No books with this ISBN found."});
+    }
+    else {
+        res.send(books[isbn])
+    }
 });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  const author = req.params.author
-  for(let i=1;i<11;i++) {
-    if(books[i].author == author) {
-        res.send(books[i])
-        };
+    const author = req.params.author
+    for(let i=1;i<11;i++) {
+        if(!books[i]) {
+            break
+        }
+        else {
+            if(books[i].author == author) {
+                res.send(books[i])
+                }
+            else if (i == 10) {
+                return res.status(404).json({message: "No books of this Author found."});
+            }
+        }
     };
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
     const title = req.params.title;
-    for(let i=1;i<11;i++) {
-        if(books[i].title == title) {
-            res.send(books[i])
-        };
+    for(let i=1;i<i+1;i++) {
+        if(!books[i]) {
+            break
+        }
+        else {
+            if(books[i].title == title) {
+                res.send(books[i])
+            }
+            else if (i == 10) {
+                return res.status(404).json({message: "No books of this Title found."});
+            }
+        }
     };
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
     const isbn = req.params.isbn;
-    res.send(books[isbn].reviews)
+    if (!books[isbn]) {
+        return res.status(404).json({message: "No books with this ISBN found."});
+    }
+    else {
+        res.send(books[isbn].reviews)
+    }
 });
 
 module.exports.general = public_users;
